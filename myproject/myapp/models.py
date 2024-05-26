a13db6ec4620f3cb717ca0655319be312c582c98
Hamedalabri <alabri.hamed1@gmail.com>
2024-05-26 16:50:12 +0200

    now

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -19,11 +19,11 @@ class Auteur(models.Model):
 
 class Jeu(models.Model):
     titre = models.CharField(max_length=200)
-    annee_sortie = models.IntegerField()
+    annee_sortie = models.IntegerField(default=2000)
     photo_boite = models.ImageField(upload_to='photos/', blank=True, null=True)
     editeur = models.CharField(max_length=200)
-    auteur = models.ForeignKey(Auteur, on_delete=models.CASCADE)
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    auteur = models.ForeignKey(Auteur, on_delete=models.CASCADE, blank=True, null=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.titre
