43fec5ee6610fd74542aeb2240429992e045d549
Hamedalabri <alabri.hamed1@gmail.com>
2024-05-26 14:55:48 +0200

    k

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,43 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+
+    # categories
+    path('categories/', views.categorieList, name='categorie-liste'),
+    path('categories/create/', views.categorie_create, name='create_categorie'),
+    path('categories/update/<int:id>/', views.categorie_update, name='update_categorie'),
+    path('categories/delete/<int:id>/', views.categorie_delete, name='delete_categorie'),
+    path('categories/<int:categorie_id>/', views.display_categorie, name='display_categorie'),
+
+    # auteurs
+    path('auteurs/', views.auteurList, name='auteur-liste'),
+    path('auteurs/create/', views.auteur_create, name='create_auteur'),
+    path('auteurs/update/<int:id>/', views.auteur_update, name='update_auteur'),
+    path('auteurs/delete/<int:id>/', views.auteur_delete, name='delete_auteur'),
+    path('auteurs/<int:auteur_id>/', views.display_auteur, name='display_auteur'),
+
+    # jeux
+    path('jeux/', views.jeuList, name='jeu-liste'),
+    path('jeux/create/', views.jeu_create, name='create_jeu'),
+    path('jeux/update/<int:id>/', views.jeu_update, name='update_jeu'),
+    path('jeux/delete/<int:id>/', views.jeu_delete, name='delete_jeu'),
+    path('jeux/<int:jeu_id>/', views.display_jeu, name='display_jeu'),
+    path('jeu-list/categorie/<str:categorie_nom>/', views.jeux_liste_par_categorie, name='jeux-liste-par-categorie'),
+
+    # joueurs
+    path('joueurs/', views.joueurList, name='joueur-liste'),
+    path('joueurs/create/', views.joueur_create, name='create_joueur'),
+    path('joueurs/update/<int:id>/', views.joueur_update, name='update_joueur'),
+    path('joueurs/delete/<int:id>/', views.joueur_delete, name='delete_joueur'),
+    path('joueurs/<int:joueur_id>/', views.display_joueur, name='display_joueur'),
+
+    # commentaires
+    path('commentaires/', views.commentaireList, name='commentaire-liste'),
+    path('commentaires/create/', views.commentaire_create, name='create_commentaire'),
+    path('commentaires/update/<int:id>/', views.commentaire_update, name='update_commentaire'),
+    path('commentaires/delete/<int:id>/', views.commentaire_delete, name='delete_commentaire'),
+    path('commentaires/<int:commentaire_id>/', views.display_commentaire, name='display_commentaire'),
+]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Categorie, Auteur, Jeu, Joueur, Commentaire
 from .forms import CategorieForm, AuteurForm, JeuForm, JoueurForm, CommentaireForm
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
 
 # les catégories
-
+def display_categorie(request, categorie_id):
+    categorie = get_object_or_404(Categorie, pk=categorie_id)
+    return render(request, 'categorie/display_categorie.html', {'categorie': categorie})
 
 def categorieList(request):
     categories = Categorie.objects.all()
-    return render(request,'categorie_liste.html',{'categories': categories})
-
+    return render(request,'categorie/categorie_liste.html',{'categories': categories})
 
 def categorie_create(request):
     if request.method == 'POST':
@@ -18,10 +23,10 @@ def categorie_create(request):
             return redirect('categorie-liste')
     else:
         form = CategorieForm()
-    return render(request, 'categorie_create.html', {'form': form})
+    return render(request, 'categorie/categorie_create.html', {'form': form})
 
 def categorie_update(request, id):
-    categorie = get_object_or_404(Categorie, id=id)
+    categorie = get_object_or_404(Categorie, pk=id)
     if request.method == 'POST':
         form = CategorieForm(request.POST, instance=categorie)
         if form.is_valid():
@@ -29,25 +34,26 @@ def categorie_update(request, id):
             return redirect('categorie-liste')
     else:
         form = CategorieForm(instance=categorie)
-    return render(request, 'categorie_create.html', {'form': form})
+    return render(request, 'categorie/categorie_update.html', {'form': form})
 
 def categorie_delete(request, id):
     categorie = get_object_or_404(Categorie, pk=id)
     categorie.delete()
-    return render('categorie-liste')
+    return redirect('categorie-liste')
 
-
-
-
-
-
-
+def jeux_liste_par_categorie(request, categorie_nom):
+    categorie = Categorie.objects.get(nom=categorie_nom)
+    jeux = Jeu.objects.filter(categorie=categorie)
+    return render(request, 'jeu/jeux_liste.html', {'jeux': jeux, 'categorie': categorie})
 
 # les auteurs
+def display_auteur(request, auteur_id):
+    auteur = get_object_or_404(Auteur, pk=auteur_id)
+    return render(request, 'auteur/display_auteur.html', {'auteur': auteur})
 
-def aueturList(request):
+def auteurList(request):
     auteurs = Auteur.objects.all()
-    return render(request,'auteur_liste.html',{'auteurs': auteurs})
+    return render(request,'auteur/auteur_liste.html',{'auteurs': auteurs})
 
 def auteur_create(request):
     if request.method == 'POST':
@@ -57,10 +63,10 @@ def auteur_create(request):
             return redirect('auteur-liste')
     else:
         form = AuteurForm()
-    return render(request, 'auteur_create.html', {'form': form})
+    return render(request, 'auteur/auteur_create.html', {'form': form})
 
 def auteur_update(request, id):
-    auteur = get_object_or_404(Auteur, id=id)
+    auteur = get_object_or_404(Auteur, pk=id)
     if request.method == 'POST':
         form = AuteurForm(request.POST, instance=auteur)
         if form.is_valid():
@@ -68,56 +74,56 @@ def auteur_update(request, id):
             return redirect('auteur-liste')
     else:
         form = AuteurForm(instance=auteur)
-    return render(request, 'auteur_update.html', {'form': form})
+    return render(request, 'auteur/auteur_update.html', {'form': form})
 
 def auteur_delete(request, id):
     auteur = get_object_or_404(Auteur, pk=id)
     auteur.delete()
-    return render('auteur-liste')
-
-
-
-
+    return redirect('auteur-liste')
 
 # les jeux
+def display_jeu(request, jeu_id):
+    jeu = get_object_or_404(Jeu, pk=jeu_id)
+    return render(request, 'jeu/display_jeu.html', {'jeu': jeu})
 
 def jeuList(request):
     jeux = Jeu.objects.all()
-    return render(request,'jeux_liste.html',{'jeux': jeux})
+    return render(request,'jeu/jeux_liste.html',{'jeux': jeux})
 
 def jeu_create(request):
     if request.method == 'POST':
         form = JeuForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('jeux-liste')
+            return redirect('jeu-liste')
     else:
         form = JeuForm()
-    return render(request, 'jeu_create.html', {'form': form})
+    return render(request, 'jeu/jeu_create.html', {'form': form})
 
 def jeu_update(request, id):
-    jeu = get_object_or_404(Jeu, id=id)
+    jeu = get_object_or_404(Jeu, pk=id)
     if request.method == 'POST':
         form = JeuForm(request.POST, instance=jeu)
         if form.is_valid():
             form.save()
-            return redirect('jeux-liste')
+            return redirect('jeu-liste')
     else:
         form = JeuForm(instance=jeu)
-    return render(request, 'jeu_update.html', {'form': form})
+    return render(request, 'jeu/jeu_update.html', {'form': form})
 
 def jeu_delete(request, id):
     jeu = get_object_or_404(Jeu, pk=id)
     jeu.delete()
-    return render('Jeux-liste')
+    return redirect('jeu-liste')
 
-
-
-#les joueurs
+# les joueurs
+def display_joueur(request, joueur_id):
+    joueur = get_object_or_404(Joueur, pk=joueur_id)
+    return render(request, 'joueur/display_joueur.html', {'joueur': joueur})
 
 def joueurList(request):
     joueurs = Joueur.objects.all()
-    return render(request,'joueur_liste.html',{'joueurs': joueurs})
+    return render(request,'joueur/joueur_liste.html',{'joueurs': joueurs})
 
 def joueur_create(request):
     if request.method == 'POST':
@@ -127,10 +133,10 @@ def joueur_create(request):
             return redirect('joueur-liste')
     else:
         form = JoueurForm()
-    return render(request, 'joueur_create.html', {'form': form})
+    return render(request, 'joueur/joueur_create.html', {'form': form})
 
 def joueur_update(request, id):
-    joueur = get_object_or_404(Joueur, id=id)
+    joueur = get_object_or_404(Joueur, pk=id)
     if request.method == 'POST':
         form = JoueurForm(request.POST, instance=joueur)
         if form.is_valid():
@@ -138,18 +144,17 @@ def joueur_update(request, id):
             return redirect('joueur-liste')
     else:
         form = JoueurForm(instance=joueur)
-    return render(request, 'joueur_update.html', {'form': form})
+    return render(request, 'joueur/joueur_update.html', {'form': form})
 
 def joueur_delete(request, id):
-    joueur = get_object_or_404(Joueur, id=id)
+    joueur = get_object_or_404(Joueur, pk=id)
     joueur.delete()
-    return render('joueur-liste')
-
-
-
-
+    return redirect('joueur-liste')
 
 # les commentaires
+def display_commentaire(request, commentaire_id):
+    commentaire = get_object_or_404(Commentaire, pk=commentaire_id)
+    return render(request, 'display_commentaire.html', {'joueur': commentaire})
 
 def commentaireList(request):
     commentaires = Commentaire.objects.all()
@@ -166,7 +171,7 @@ def commentaire_create(request):
     return render(request, 'commentaire_create.html', {'form': form})
 
 def commentaire_update(request, id):
-    commentaire = get_object_or_404(Commentaire, id=id)
+    commentaire = get_object_or_404(Commentaire, pk=id)
     if request.method == 'POST':
         form = CommentaireForm(request.POST, instance=commentaire)
         if form.is_valid():
@@ -177,6 +182,6 @@ def commentaire_update(request, id):
     return render(request, 'commentaire_update.html', {'form': form})
 
 def commentaire_delete(request, id):
-    commentaire = get_object_or_404(Commentaire, id=id)
+    commentaire = get_object_or_404(Commentaire, pk=id)
     commentaire.delete()
-    return render('commentaire-liste')
+    return redirect('commentaire-liste')
